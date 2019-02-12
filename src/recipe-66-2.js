157e38a76d166f9ed3ceed9c164e9a822d9275d7
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-02-12 22:19:46 +0900

    すごい中途半端だけれど本日の作業分

---
+++ src/recipe-66-2.js
@@ -0,0 +1,13 @@
+const array1 = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10];
+const shuffled = shuffledArray(array1);
+console.log(shuffled);
+
+const array2 = ['田中','鈴木','吉田','辻',’後藤’]
+const shuffled2 = shuffledArray(array1);
+console.log(shuffled2);
+function shuffleArray(sorceArr){
+  const array= sourceArr.concat();
+
+  const arrayLength= array.length;
+  for(let)
+}
