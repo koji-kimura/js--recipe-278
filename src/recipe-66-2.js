982c7669eb83d70cba0535f2678c26407323b997
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-02-13 22:25:08 +0900

    また中途半端なところまでだけど

--- src/recipe-66-2.js
+++ src/recipe-66-2.js
@@ -2,12 +2,17 @@ const array1 = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10];
 const shuffled = shuffledArray(array1);
 console.log(shuffled);
 
-const array2 = ['田中','鈴木','吉田','辻',’後藤’]
-const shuffled2 = shuffledArray(array1);
+const array2 = ["田中", "鈴木", "吉田", "辻"];
+const shuffled2 = shuffledArray(array2);
 console.log(shuffled2);
-function shuffleArray(sorceArr){
-  const array= sourceArr.concat();
+function shuffledArray(sorceArr) {
+  const array = sourceArr.concat();
 
-  const arrayLength= array.length;
-  for(let)
+  const arrayLength = array.length;
+  for (let i = arrayLength - 1; i >= 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+
+  return array;
 }
